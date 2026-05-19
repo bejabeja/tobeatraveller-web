5eeccda882b47203ce0df85789d94ebe10919a5d
bejabeja <miriam.abella211@gmail.com>
2026-05-19 19:40:58 +0200

    perf: split vendor bundles to reduce main chunk size

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,25 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-redux': ['redux', 'react-redux', '@reduxjs/toolkit', 'redux-thunk'],
+          'vendor-map': ['leaflet', 'react-leaflet'],
+          'vendor-ui': ['react-icons', 'react-hot-toast'],
+          'vendor-forms': ['react-hook-form', '@hookform/resolvers', 'zod'],
+          'vendor-utils': ['lodash'],
+        }
+      }
     }
   }
 })
